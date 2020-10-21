d5fabe2a0c8871315d1b561732d4038cc3444ca2
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-10-21 20:59:33 +0530

    change5

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,27 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      para: false
+    };
   }
-
+  para() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
   render() {
     return (
       <>
-        <div id="main">{/* Do not remove this main div!! */}</div>
-        <button
-          id="click"
-          onClick={() => (
-            <p id="para">
-              Hello, I've learnt to use the full-stack evaluation tool. This
-              makes me so happy
-            </p>
-          )}
-        >
-          hello
-        </button>
+        <div id="main">
+          {/* Do not remove this main div!! */}
+          <button id="click" onClick={() => this.setState({ para: true })}>
+            {this.state.para ? this.fun() : ""}
+          </button>
+        </div>
       </>
     );
   }
